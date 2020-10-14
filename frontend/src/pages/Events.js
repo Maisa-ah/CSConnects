53319b80111135c6b417bf4a189f81540acb9ea4
Maisa Ahmad <2013maisaah@gmail.com>
2020-10-14 14:01:22 -0400

    editted navBar

--- frontend/src/pages/Events.js
+++ frontend/src/pages/Events.js
@@ -1,9 +1,10 @@
 import React from 'react';
 import Navigation from '../components/Navigation';
+import Computer from '../images/comp.svg';
 
 function EventsPage(){
   return(
-    <Navigation/>
+    <Navigation icon={Computer} name="CSConnects"/>
   );
 
 }
